126b0d00f4c73946286cd8448e712f445896f1dd
Zihao-hub <huanghao_1027@163.com>
2026-03-22 18:21:46 +0800

    Update pointnet_part_seg.py

--- models/pointnet_part_seg.py
+++ models/pointnet_part_seg.py
@@ -9,27 +9,35 @@ from pointnet_utils import STN3d, STNkd, feature_transform_reguliarzer
 class get_model(nn.Module):
     def __init__(self, part_num=50, normal_channel=True):
         super(get_model, self).__init__()
-        if normal_channel:
-            channel = 6
-        else:
-            channel = 3
+        
+   
+
+        # xyzrgb + G_g^n + G_c^n 
+        channel = 8  # 
+
         self.part_num = part_num
-        self.stn = STN3d(channel)
+        self.stn = STN3d(channel)  
+        
         self.conv1 = torch.nn.Conv1d(channel, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 128, 1)
         self.conv4 = torch.nn.Conv1d(128, 512, 1)
         self.conv5 = torch.nn.Conv1d(512, 2048, 1)
+        
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
         self.bn3 = nn.BatchNorm1d(128)
         self.bn4 = nn.BatchNorm1d(512)
         self.bn5 = nn.BatchNorm1d(2048)
+        
         self.fstn = STNkd(k=128)
+        
+
         self.convs1 = torch.nn.Conv1d(4944, 256, 1)
         self.convs2 = torch.nn.Conv1d(256, 256, 1)
         self.convs3 = torch.nn.Conv1d(256, 128, 1)
         self.convs4 = torch.nn.Conv1d(128, part_num, 1)
+        
         self.bns1 = nn.BatchNorm1d(256)
         self.bns2 = nn.BatchNorm1d(256)
         self.bns3 = nn.BatchNorm1d(128)
@@ -38,6 +46,8 @@ class get_model(nn.Module):
         B, D, N = point_cloud.size()
         trans = self.stn(point_cloud)
         point_cloud = point_cloud.transpose(2, 1)
+        
+
         if D > 3:
             point_cloud, feature = point_cloud.split(3, dim=2)
         point_cloud = torch.bmm(point_cloud, trans)
@@ -63,13 +73,15 @@ class get_model(nn.Module):
         out_max = torch.cat([out_max,label.squeeze(1)],1)
         expand = out_max.view(-1, 2048+16, 1).repeat(1, 1, N)
         concat = torch.cat([expand, out1, out2, out3, out4, out5], 1)
+        
         net = F.relu(self.bns1(self.convs1(concat)))
         net = F.relu(self.bns2(self.convs2(net)))
         net = F.relu(self.bns3(self.convs3(net)))
         net = self.convs4(net)
+        
         net = net.transpose(2, 1).contiguous()
         net = F.log_softmax(net.view(-1, self.part_num), dim=-1)
-        net = net.view(B, N, self.part_num) # [B, N, 50]
+        net = net.view(B, N, self.part_num)
 
         return net, trans_feat
 
